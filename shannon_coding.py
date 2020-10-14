6d21cbba9f0fd138b4b914d1379a0c8e9032e102
positron <vasanth.mani.vasi.2@gmail.com>
2020-10-14 20:57:28 +0530

    Integrated shannon coding

--- shannon_coding.py
+++ shannon_coding.py
@@ -1,31 +1,32 @@
 import utils
-from other_formulas import *
+from formulas import *
 
 def shannon_coding(probabilities):
-    idx_to_bin = {}
+    answer = {}
+    answer['idx_to_bin'] = {}
     probabilities.sort(reverse=True)
     for i in range(len(probabilities)):
-        idx_to_bin[i] = ''
-    utils.separate(probabilities, idx_to_bin)
+        answer['idx_to_bin'][i] = ''
+    utils.separate(probabilities, answer['idx_to_bin'])
 
-    idx_to_len = {k: len(v) for k,v in idx_to_bin.items()}
+    answer['idx_to_len'] = {k: len(v) for k,v in answer['idx_to_bin'].items()}
 
-    H = entropy(probabilities)
-    lavg = Lavg(probabilities=probabilities, lengths=idx_to_len.values())
-    eff = efficiency(H, lavg)
-    red = redundancy(eff)
-    cv = code_variance(probs=probabilities, lengths=idx_to_len.values(), lavg=lavg)
+    answer['H'] = entropy(probabilities)
+    answer['lavg'] = Lavg(probabilities=probabilities, lengths=answer['idx_to_len'].values())
+    answer['eff'] = efficiency(answer['H'], answer['lavg'])
+    answer['red'] = redundancy(answer['eff'])
+    answer['cv'] = code_variance(probs=probabilities, lengths=answer['idx_to_len'].values(), lavg=answer['lavg'])
 
-    return idx_to_bin, idx_to_len, H, lavg, eff, red, cv
+    return answer
     
 if __name__ == '__main__':
     probs = input("Enter probabilities: ")
     probs = list(map(float, probs.split()))
-    idx_to_bin, idx_to_len, H, lavg, eff, red, cv = shannon_coding(probs)
-    for prob, bin, length in zip(probs, idx_to_bin.values(), idx_to_len.values()):
-        print(prob, '\t', bin, '\t', length) 
-    print("Entropy:", H)
-    print("Average length:", lavg)
-    print("Efficiency:", eff)
-    print("Redundancy:", red)
-    print("Code Variance:", cv)
+    answer = shannon_coding(probs)
+    for prob, bin, length in zip(probs, answer['idx_to_bin'].values(), answer['idx_to_len'].values()):
+        print(prob, '\t', bin, '\t', length)
+    print("Entropy:", answer['H'])
+    print("Average length:", answer['lavg'])
+    print("Efficiency:", answer['eff'])
+    print("Redundancy:", answer['red'])
+    print("Code Variance:", answer['cv'])
